a4749805610502b3c5fa5edbce8c8429f00b96c0
miky-shin <mkshin920@gmail.com>
2021-03-08 23:31:44 +0900

    authentication

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ import schema from "./schema";
 const PORT = process.env.PORT;
 const server = new ApolloServer({
     schema,
+    context: {
+        Authorization:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE1MjA3NzgwfQ.VG9D743GwUpSvGEXbXiR8uFfKADvEKvG1bg6lRqrN6A"
+    },
 });
 
 server.listen(PORT).then(()=> console.log(`🚀 Server is running on http://localhost:${PORT} 🧡`));
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,19 +1,23 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../../client";
 
 export default {
   Mutation: {
     editProfile: async (
       _,
-      { firstName, lastName, username, email, password: newPassword }
+      { firstName, lastName, username, email, password: newPassword },
+      { Authorization }
     ) => {
+
+      const { id } = await jwt.verify(Authorization, process.env.SECRET_KEY);
       let uglyPassword = null;
       if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10);
       }
       const updatedUser = await client.user.update({
         where: {
-          id: 1,
+          id,
         },
         data: {
           firstName,
