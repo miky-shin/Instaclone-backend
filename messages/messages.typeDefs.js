42b133c843d599e672dbac33983307aeb384f99b
miky-shin <mkshin920@gmail.com>
2021-03-20 00:28:26 +0900

    send message

---
+++ messages/messages.typeDefs.js
@@ -0,0 +1,19 @@
+import { gql } from "apollo-server";
+
+export default gql`
+  type Room {
+    id: Int!
+    users: [User]
+    messages: [Message]
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Message {
+    id: Int!
+    payload: String!
+    user: User!
+    room: Room!
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
