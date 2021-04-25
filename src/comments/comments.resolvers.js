6398f91e1d2036e65a64b71364a7a6fc58872633
miky-shin <mkshin920@gmail.com>
2021-04-25 10:00:44 +0900

    comment user

--- src/comments/comments.resolvers.js
+++ src/comments/comments.resolvers.js
@@ -1,5 +1,6 @@
 export default {
   Comment: {
+    user: ({ userId }) => client.user.findUnique({ where: { id: userId } }),
     isMine: ({ userId }, _, { loggedInUser }) => {
       if (!loggedInUser) {
           console.log(userId);
